7d622a0000da605488c7f3960d69a0656f6c7329
Sharif-00112 <sharif.hossain02@northsouth.edu>
2022-09-23 21:12:00 +0600

    DELETE

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -9,6 +9,27 @@ const Products = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     } ,[])
+
+    //DELETE a product 
+    const handleDeleteProduct = id =>{
+        const proceed = window.confirm('Are you sure you want to delete?');
+        if(proceed){
+            const url = `http://localhost:3001/products/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0){
+                    alert('Deleted Successfully!')
+
+                    // update the UI (or, set [products] dependency instead of it)
+                    const remainingProducts = products.filter(product => product._id !== id);
+                    setProducts(remainingProducts);
+                }
+            })
+        }
+    }
     
     return (
         <div>
@@ -20,6 +41,15 @@ const Products = () => {
                     products.map(product => <li key={product._id}>
                         {product.prodTitle} - {product.color} - {product.price} TK - {product.stock} {product.uom} in Stock
 
+                        <button onClick={() =>handleDeleteProduct(product._id)}>
+                            Delete
+                        </button>
+
+                        <Link to={`products/update/${product._id}`}>
+                            <button>
+                                Update
+                            </button>
+                        </Link>
                     </li>)
                 }
             </ul>
